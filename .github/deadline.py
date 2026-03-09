0b056c33af45f36d7a3d2165fad97107dfcfd8de
Kuznetsov-Artyom <artyomka_working@mail.ru>
2026-03-09 16:33:05 +0300

    remove logger
    Signed-off-by: Kuznetsov-Artyom <artyomka_working@mail.ru>

--- .github/deadline.py
+++ .github/deadline.py
@@ -1,12 +1,8 @@
 import os
 import sys
-import logging
 from github import Github
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
-
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-logger = logging.getLogger(__name__)
 
 
 def main():
@@ -25,10 +21,7 @@ def main():
     current_labels = [label.name for label in pr.get_labels()]
     lab_label = next((l for l in current_labels if l in lab_labels), None)
 
-    logger.log(f"Repository: {repo}, PR: {pr}, Labels: {current_labels}")
-
     if not lab_label:
-        logger.info("Lab label not found")
         return
 
     pr_created_date = pr.created_at.astimezone(moscow_tz)
@@ -38,21 +31,13 @@ def main():
     LABEL_LOW_PRIORITY = "low priority"
     LABEL_DELAYED = "delayed"
 
-    logger.info(
-        f"Lab: {lab_label}, Created: {pr_created_date}, Deadline: {deadline_date}"
-    )
-    logger.info(f"Time before deadline: {time_before_deadline}")
-
     if current_date > deadline_date:
         if LABEL_DELAYED not in current_labels:
-            logger.info("Adding label: delayed")
             pr.add_to_labels(LABEL_DELAYED)
         if LABEL_LOW_PRIORITY in current_labels:
-            logger.info("Removing label: low priority")
             pr.remove_from_labels(LABEL_LOW_PRIORITY)
     elif time_before_deadline < timedelta(hours=72):
         if LABEL_LOW_PRIORITY not in current_labels:
-            logger.info("Adding label: low priority")
             pr.add_to_labels(LABEL_LOW_PRIORITY)
 
 
